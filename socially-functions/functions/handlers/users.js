b23db51de85020069bb693967756b478199375fa
jscode <jscslg27@gmail.com>
2021-04-14 17:34:01 +0530

    bug fixes

--- socially-functions/functions/handlers/users.js
+++ socially-functions/functions/handlers/users.js
@@ -143,10 +143,11 @@ exports.getUserDetails = (req, res) => {
 exports.getAuthenticatedUser = (req,res) => {
 	let userData = {};
 	db.doc(`/users/${req.user.handle}`).get()
-		.then(doc => {
+		.then((doc) => {
+            console.log(doc.data)
 			if(doc.exists){
 				userData.credentials = doc.data();
-				return db.collection('likes').where('userHandle','==',req.user.hamdle).get();
+				return db.collection("likes").where("userHandle", "==", req.user.handle).get();
 			}
 		})
 		.then(data => {
@@ -154,12 +155,31 @@ exports.getAuthenticatedUser = (req,res) => {
 			data.forEach(doc => {
 				userData.likes.push(doc.data());
 			})
-			return res.json(userData);
-		})
-		.catch(err => {
-			console.error(err);
-			return res.status(500).json({error: err.code});
-		})
+			return db.collection("notifications")
+                    .where("recipient", "==", req.user.handle)
+                    .orderBy("createdAt", "desc")
+                    .limit(10)
+                    .get();
+    })
+    .then((data) => {
+      userData.notifications = [];
+      data.forEach((doc) => {
+        userData.notifications.push({
+          recipient: doc.data().recipient,
+          sender: doc.data().sender,
+          createdAt: doc.data().createdAt,
+          screamId: doc.data().screamId,
+          type: doc.data().type,
+          read: doc.data().read,
+          notificationId: doc.id,
+        });
+      });
+      return res.json(userData);
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).json({ error: err.code });
+    });
 }
 
 exports.uploadImage = (req, res) => {
