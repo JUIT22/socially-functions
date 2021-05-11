52f3defd4bd30efa714ba2f2bd2e4dd949538f00
jscode <jscslg27@gmail.com>
2021-05-12 00:40:28 +0530

    file structure changed

--- socially-functions/functions/util/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    apiKey: "AIzaSyBfiChOSRUmtiJB-YU57yxMxVv067XmsU0",
-    authDomain: "juit-socially.firebaseapp.com",
-    projectId: "juit-socially",
-    storageBucket: "juit-socially.appspot.com",
-    messagingSenderId: "625307113396",
-    appId: "1:625307113396:web:f04f0fa4f0da66642a04e1",
-    measurementId: "G-L0WZGQ5L35"
-};
